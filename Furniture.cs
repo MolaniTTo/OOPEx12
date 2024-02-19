1627c6c6e97f5f6727861ee56b7f715c07861745
arnauitb <arnau.molano.7e7@itb.cat>
2024-02-19 11:37:57 +0100

    Add project files.

---
+++ Furniture.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace OOPEx12
+{
+    public class Furniture
+    {
+        private float weight;
+        private double price;
+        private bool set;
+        private string name, material, colour;
+
+
+        public float Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public bool Set
+        {
+            get { return set; }
+            set { set = value; }
+        }   
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Material
+        {
+            get { return material; }
+            set { material = value; }
+        }
+        public string Colour
+        {
+            get { return colour; }
+            set { colour = value; }
+        }
+
+        public Furniture(float weight, double price , bool set, string name, string material, string colour)
+        {
+            Weight = weight;
+            Price = price;
+            Set = set;
+            Name = name;
+            Material = material;
+            Colour = colour;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Weight: {Weight}, Price: {Price}, Set: {Set}, Name: {Name}, Material: {Material}, Colour: {Colour}";
+        }
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using OOPEx12;
+
+public class Program
+{
+    public static void Main()
+    {
+        Furniture furniture = new Furniture(125 ,250.99, true, "Arnau", "Acero", "Gris" );
+
+        Console.WriteLine(furniture.ToString());
+
+    }
+}
